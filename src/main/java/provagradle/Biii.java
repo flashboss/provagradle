931b96dbc8c97c992b8fbbb74136b124016fc4b6
Luca Stancapiano <lucastancapiano@MBP-di-Luca.wind3.hub>
2021-04-14 14:21:29 +0200

    start

---
+++ src/main/java/provagradle/Biii.java
@@ -0,0 +1,11 @@
+package provagradle;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.slf4j.Logger;
+
+public class Biii {
+
+	private Logger logger = getLogger(Biii.class);
+
+}
---
+++ src/test/java/provagradle/Buuuuuuuu.java
@@ -0,0 +1,12 @@
+package provagradle;
+
+import org.junit.jupiter.api.Test;
+
+public class Buuuuuuuu {
+
+	@Test
+	public void test() throws Exception {
+		new Biii();
+	}
+
+}
